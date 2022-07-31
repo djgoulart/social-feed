ae4934cb079bd5de15f3ddede8cc7ed51780e7c7
Diego Goulart <diego.goulart@edifyeducation.com.br>
2022-07-31 16:15:55 -0300

    chore: eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
-import { Header } from './components/Header';
+import { Header } from './components/Header'
 
 function App() {
-
   return (
     <div className="App">
       <Header />
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react'
 
-import styles from "./Header.module.css";
+import styles from './Header.module.css'
 
 export function Header() {
   return (
