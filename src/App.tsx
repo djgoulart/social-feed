306d84790dfc33ba67accd0fc12d4089fa20279e
Diego Goulart <diego.goulart@edifyeducation.com.br>
2022-07-31 18:09:03 -0300

    feat: sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
 import './global.css'
+import styles from './App.module.css'
 
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>posts</main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/10280312?v=4"
+          alt=""
+        />
+
+        <strong>Diego Goulart</strong>
+        <span>Software Engineer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
